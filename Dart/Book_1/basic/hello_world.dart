ebc15b4ff79e3901092d680576572286062cd2f7
Pruthviraj <technazro@gmail.com>
2023-10-11 12:39:38 +0530

    created hello world in Dart programing

---
+++ Dart/Book_1/basic/hello_world.dart
@@ -0,0 +1,8 @@
+void main() => print("Hello world in Dart Programming");
+
+
+/*
+
+main function is an entry point in Dart program, means the main function executs first and then code written inside main  executes
+
+*/
